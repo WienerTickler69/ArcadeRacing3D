e6daf0f9821377513205a65f115a360d1a944bf3
WienerTickler_69 <oliverlillywhite05@gmail.com>
2021-11-11 15:39:19 +0000

    Initial github Push

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour {
+
+    public GameObject CountDown;
+    public AudioSource GetReady;
+    public AudioSource GoAudio;
+    public GameObject LapTimer;
+    public GameObject CarControls;
+
+    void Start()
+    {
+
+    }
+
+} 
